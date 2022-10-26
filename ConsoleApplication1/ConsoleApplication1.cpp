34f48d94760dec1f4ae73bb6e55bed6485161bcd
Mutter <Mutter@DESKTOP-GNKJNPM>
2022-10-26 12:07:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+  string a = "egwwegeg";
+
+  string suba = "eg";
+
+  int c = 0;
+
+  for (int i = 0; i < a.length(); i++)
+  {
+    if (a[i] == suba[0])
+    {
+      if (a.substr(i, suba.length()) == suba)
+      {
+        c++;
+        i += suba.length() - 1;
+      }
+    }
+  }
+
+  cout << c;
+
+}
